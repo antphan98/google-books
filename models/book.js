ca457de4df5cc8da116fa15fcae3958d680770ba
An Phan <antphan98@gmail.com>
2020-02-24 12:18:25 -0600

    fixed server.js issue

--- models/book.js
+++ models/book.js
@@ -4,9 +4,9 @@ const Schema = mongoose.Schema;
 const bookSchema = new Schema({
   title: { type: String, required: true },
   author: { type: String, required: true },
-  description: { type: String},
+  description: { type: String },
   image: { type: String },
-  link: { type: String}
+  link: { type: String }
 });
 
 const Book = mongoose.model("Book", bookSchema);

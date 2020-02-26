2c5262fd61658b5eb297452b9b9a396b522b20e3
An Phan <antphan98@gmail.com>
2020-02-25 19:53:52 -0600

    fixed proxy issue/favicon

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import Wrapper from './components/Wrapper'
 
 function App() {
   return (
-    <div>
-      <h1>hello</h1>
-    </div>
+    <Wrapper>
+      <Navbar>
+      
+      </Navbar>
+    </Wrapper>
   );
 }
 
